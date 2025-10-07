b453ff19f25579ea0736f5fd2ac08401b024fe24
João Vitor <joaovitor.teixeira@a.ucb.br>
2025-10-07 10:16:30 -0300

    Initial project setup with musical instrument classes
    Adicionar configuração Maven, .gitignore e arquivos de projeto IntelliJ IDEA. Implementar a classe abstrata base Instrumentos e a classe concreta Violao. Incluir uma classe Main com um ponto de entrada básico.

---
+++ src/main/java/org/example/Instrumentos.java
@@ -0,0 +1,43 @@
+package org.example;
+public abstract class Instrumentos {
+    protected String nome;
+    protected String material;
+
+    public Instrumentos(String nome, String material)
+    {
+        this.nome = nome;
+        this.material = material;
+
+    }
+
+    public abstract void tocar();
+
+    public void afinar(){
+        System.out.println("Afinando o "+ nome);
+    }
+
+    public void mostrarInformacoes()
+    {
+        System.out.println("Instrumento: " + nome);
+        System.out.println("Material: "+ material);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
+
+
+
